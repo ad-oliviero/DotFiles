ca803bc938e26e7e3b8caae4bf4acf23870512d6
TheDarkBug <adrianoliviero23@gmail.com>
2021-03-08 12:56:15 +0100

    Added install with symlinks, updated to ayu color scheme.

---
+++ install-ln.sh
@@ -0,0 +1,43 @@
+rm -rf \
+		~/.config/alacritty \
+		~/.config/aliasrc \
+		~/.config/dunst \
+		~/.config/htop \
+		~/.config/i3 \
+		~/.config/kitty \
+		~/.config/mako \
+		~/.config/picom \
+		~/.config/polybar \
+		~/.config/rofi \
+		~/.config/scripts \
+		~/.config/sway \
+		~/.config/swaylock \
+		~/.oh-my-zsh \
+		~/.vim* \
+		~/.xmonad \
+		~/.zshrc \
+		~/.zsh_history
+
+ln -s \
+		~/.config/DotFiles/alacritty \
+		~/.config/DotFiles/aliasrc \
+		~/.config/DotFiles/dunst \
+		~/.config/DotFiles/htop \
+		~/.config/DotFiles/i3 \
+		~/.config/DotFiles/kitty \
+		~/.config/DotFiles/mako \
+		~/.config/DotFiles/picom \
+		~/.config/DotFiles/polybar \
+		~/.config/DotFiles/rofi \
+		~/.config/DotFiles/scripts \
+		~/.config/DotFiles/sway \
+		~/.config/DotFiles/swaylock \
+		~/.config
+ln -s \
+		~/.config/DotFiles/.oh-my-zsh \
+		~/.config/DotFiles/.vim* \
+		~/.config/DotFiles/.xmonad \
+		~/.config/DotFiles/.zshrc \
+		~/.config/DotFiles/.zsh_history \
+		~/
+
