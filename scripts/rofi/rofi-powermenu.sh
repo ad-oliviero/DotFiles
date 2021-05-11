0c84fa084528c0fc4707f6b538c8d885f4be5a97
TheDarkBug <adrianoliviero23@gmail.com>
2021-05-11 15:18:56 +0200

    Trying to get hibernation working

--- scripts/rofi/rofi-powermenu.sh
+++ scripts/rofi/rofi-powermenu.sh
@@ -3,16 +3,22 @@
 if [ -z "$@" ]; then
     echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
     echo -en "Reboot\0icon\x1fsystem-restart\n"
-    echo -en "FReboot\0icon\x1fsystem-restart\n"
+#	echo -en "FReboot\0icon\x1fsystem-restart\n"
     echo -en "Suspend\0icon\x1fsystem-suspend\n"
+    echo -en "Hybrid\0icon\x1fsystem-hibernate\n"
+    echo -en "Hibernate\0icon\x1fsystem-hibernate\n"
 else
     if [ "$1" = "Shutdown" ]; then
         poweroff
     elif [ "$1" = "Reboot" ]; then
         reboot
-    elif [ "$1" = "FReboot" ]; then
-        ~/.config/scripts/fast-reboot.sh
+#    elif [ "$1" = "FReboot" ]; then
+#        ~/.config/scripts/fast-reboot.sh
     elif [ "$1" = "Suspend" ]; then
         swaylock -C ~/.config/swaylock/config || ~/.config/scripts/lock & systemctl suspend
+    elif [ "$1" = "Hybrid" ]; then
+        swaylock -C ~/.config/swaylock/config || ~/.config/scripts/lock & systemctl hibernate
+    elif [ "$1" = "Hibernate" ]; then
+        swaylock -C ~/.config/swaylock/config || ~/.config/scripts/lock & sudo pm-suspend-hybrid
     fi
 fi
