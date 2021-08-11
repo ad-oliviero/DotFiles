e5d5ca2fecd6a1a4f8cda85a69e8a2a389d5d0de
TheDarkBug <adrianoliviero23@gmail.com>
2021-08-11 12:11:49 +0200

    Some updated things

--- scripts/bandwidth.sh
+++ scripts/bandwidth.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DATE1="$(date +%s%N)"
+
 init="$(($(cat /sys/class/net/[ew]*/statistics/rx_bytes | paste -sd '+')))"
 
 printf "Recording bandwidth. Press enter to stop."
@@ -8,4 +10,11 @@ read -r lol
 
 fin="$(($(cat /sys/class/net/[ew]*/statistics/rx_bytes | paste -sd '+')))"
 
-printf "%4sB of bandwidth used.\\n" $(numfmt --to=iec $(($fin-$init)))
+DATE2="$(date +%s%N)"
+
+# result="$(numfmt --to=iec $(($fin-$init)))"
+result="$(expr $(expr $fin - $init) / 1024)"
+time="$(expr $(expr $DATE2 - $DATE1) / 1000000000)"
+speed="$((expr $result / $time))"
+
+echo -e "$result used in $time\x00s at $speed"
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,6 +1,6 @@
 #[ ! "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ] && exec startx
 #[ "$(tty)" = "/dev/tty1" ] && ! pgrep -x Xorg >/dev/null && exec sway #startx -- vt2 &> /dev/null
 
-export _JAVA_AWT_WM_NONREPARENTING=1
-export MOZ_ENABLE_WAYLAND=12
-export QT_QPA_PLATFORM=wayland-egl
+#export _JAVA_AWT_WM_NONREPARENTING=1
+export MOZ_ENABLE_WAYLAND=1
+#export QT_QPA_PLATFORM=wayland-egl
