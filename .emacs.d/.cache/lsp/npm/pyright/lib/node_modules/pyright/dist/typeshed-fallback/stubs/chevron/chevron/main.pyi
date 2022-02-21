010c887c0aeb769d13c8eeda49d3d5949820a261
TheDarkBug <adrianoliviero23@gmail.com>
2022-02-21 20:25:05 +0100

    Removed emacs cache files

--- .emacs.d/.cache/lsp/npm/pyright/lib/node_modules/pyright/dist/typeshed-fallback/stubs/chevron/chevron/main.pyi
+++
@@ -1,7 +0,0 @@
-from _typeshed import StrOrBytesPath
-from typing import Any
-
-_OpenFile = StrOrBytesPath | int
-
-def main(template: _OpenFile, data: _OpenFile | None = ..., **kwargs: Any) -> str: ...
-def cli_main() -> None: ...
--- .emacs.d/.cache/lsp/npm/pyright/lib/node_modules/pyright/dist/typeshed-fallback/stubs/invoke/invoke/env.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Any
-
-class Environment:
-    data: Any
-    def __init__(self, config, prefix) -> None: ...
-    def load(self): ...
