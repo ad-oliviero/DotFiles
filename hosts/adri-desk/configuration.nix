90364071eb9dda246e4e7154718601f96d5a6c6c
TheDarkBug <adrianoliviero23@gmail.com>
2024-03-27 21:32:55 +0100

    Now using the right amd drivers on desktop

--- hosts/adri-desk/configuration.nix
+++ hosts/adri-desk/configuration.nix
@@ -3,31 +3,16 @@
     ./hardware-configuration.nix
     ../common/configuration.nix
   ];
-  boot.kernelParams = [
-    # "resume_offset=201912320"
-  ];
-  # environment.variables = {
-  #   VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
-  # };
-  # hardware.opengl.extraPackages = with pkgs; [
-  #   (
-  #     if (lib.versionOlder (lib.versions.majorMinor lib.version) "23.11")
-  #     then vaapiIntel
-  #     else intel-vaapi-driver
-  #   )
-  #   libvdpau-va-gl
-  #   intel-media-driver
-  # ];
-
   networking.hostName = "adri-desk";
 
   time.timeZone = "Europe/Rome";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  hardware.opengl.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+    amdvlk
+    driversi686Linux.amdvlk
+  ];
 
-  # nix search package_name
   environment.systemPackages = with pkgs; [
     steam
   ];
--- hosts/adri-desk/hardware-configuration.nix
+++ hosts/adri-desk/hardware-configuration.nix
@@ -9,10 +9,28 @@
     ../common/hardware-configuration.nix
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [];
+  # environment.variables = {
+  #   VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+  # };
+  # hardware.opengl.extraPackages = with pkgs; [
+  #   (
+  #     if (lib.versionOlder (lib.versions.majorMinor lib.version) "23.11")
+  #     then vaapiIntel
+  #     else intel-vaapi-driver
+  #   )
+  #   libvdpau-va-gl
+  #   intel-media-driver
+  # ];
+
+  boot = {
+    initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod"];
+    initrd.kernelModules = [];
+    kernelModules = ["kvm-amd" "amdgpu"];
+    extraModulePackages = [];
+    kernelParams = [
+      "resume_offset=1849344"
+    ];
+  };
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/e5f43164-9593-43e7-9106-d82b09ccbaec";
