7893443eb536cf685a8e7010acb52dd43907270e
Adriano Oliviero <adrianoliviero23@gmail.com>
2025-11-28 20:14:44 +0100

    Updated authorized_keys (removed hostnames)

--- terminal/.config/zsh/utils.zsh
+++ terminal/.config/zsh/utils.zsh
@@ -12,7 +12,7 @@ restow() {
       printf "\t\t\tif no option, stow everything\n"
     }>&2
   }
-  
+
   while getopts hadt flag; do
     case "${flag}" in
     h)
@@ -33,7 +33,7 @@ restow() {
   [[ ! -z "$desktop" ]] && stow desktop
   [[ ! -z "$apps" ]] && stow apps
   [[ ! -z "$terminal" ]] && stow terminal
-  [[ -z "$desktop" && -z "$apps" && -z "$terminal" ]] && stow  desktop apps terminal
+  [[ -z "$desktop" && -z "$apps" && -z "$terminal" ]] && stow desktop apps terminal
   printf "Done!\n"
   popd > /dev/null
 }
